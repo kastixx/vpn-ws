77ab0c06d08efe2e40bdbd4da7dfe7b5631e05ef
Toyam Cox <Vaelatern@gmail.com>
2017-11-13 16:20:19 -0500

    Reformat

--- src/uwsgi.c
+++ src/uwsgi.c
@@ -1,10 +1,11 @@
+#include <assert.h>
 #include "vpn-ws.h"
 
 /*
-	returns 0 if we require more data
-	returns -1 on error
-	return N as the size of the whole uwsgi packet
-*/
+   returns 0 if we require more data
+   returns -1 on error
+   return N as the size of the whole uwsgi packet
+   */
 
 ssize_t vpn_ws_uwsgi_parse(vpn_ws_peer *peer, uint8_t *modifier1, uint8_t *modifier2) {
 	if (peer->pos < 4)
@@ -12,7 +13,7 @@ ssize_t vpn_ws_uwsgi_parse(vpn_ws_peer *peer, uint8_t *modifier1, uint8_t *modif
 	*modifier1 = peer->buf[0];
 	*modifier2 = peer->buf[3];
 	uint16_t uwsgi_pktsize = vpn_ws_le16(peer->buf+1);
-	if (peer->pos < (uwsgi_pktsize + 4))
+	if (peer->pos < (uint64_t)(uwsgi_pktsize + 4))
 		return 0;
 
 	uint8_t *pkt = peer->buf+4;
@@ -20,26 +21,32 @@ ssize_t vpn_ws_uwsgi_parse(vpn_ws_peer *peer, uint8_t *modifier1, uint8_t *modif
 	ssize_t ret = 4 + uwsgi_pktsize;
 
 	while(uwsgi_pktsize) {
-		if (uwsgi_pktsize < 2) return -1;
+		if (uwsgi_pktsize < 2)
+			return -1;
 		uint16_t keylen = vpn_ws_le16(pkt); 	
 		uwsgi_pktsize -= 2;
 		pkt += 2;
-		if (keylen == 0) return -1;
-		if (keylen > uwsgi_pktsize) return -1;
+		if (keylen == 0)
+			return -1;
+		if (keylen > uwsgi_pktsize)
+			return -1;
 		char *key = (char *) pkt;
 		uwsgi_pktsize -= keylen;
 		pkt += keylen;
 
-		if (uwsgi_pktsize < 2) return -1;
+		if (uwsgi_pktsize < 2)
+			return -1;
 		uint16_t vallen = vpn_ws_le16(pkt);	
 		uwsgi_pktsize -= 2;
 		pkt += 2;
-		if (vallen > uwsgi_pktsize) return -1;
+		if (vallen > uwsgi_pktsize)
+			return -1;
 		char *val = (char *) pkt;
 		uwsgi_pktsize -= vallen;
 		pkt += vallen;
 
-		if (vpn_ws_peer_add_var(peer, key, keylen, val, vallen)) return -1;
+		if (vpn_ws_peer_add_var(peer, key, keylen, val, vallen))
+			return -1;
 	}
 
 	return ret;
@@ -48,7 +55,8 @@ ssize_t vpn_ws_uwsgi_parse(vpn_ws_peer *peer, uint8_t *modifier1, uint8_t *modif
 int vpn_ws_peer_add_var(vpn_ws_peer *peer, char *key, uint16_t keylen, char *val, uint16_t vallen) {
 	// max 64 vars
 	uint16_t pos = peer->vars_n;
-	if (pos >= 64) return -1;
+	if (pos >= 64)
+		return -1;
 
 	peer->vars[pos].key = key;
 	peer->vars[pos].keylen = keylen;
@@ -60,7 +68,8 @@ int vpn_ws_peer_add_var(vpn_ws_peer *peer, char *key, uint16_t keylen, char *val
 
 char *vpn_ws_peer_get_var(vpn_ws_peer *peer, char *key, uint16_t keylen, uint16_t *vallen) {
 	int i;
-	if (peer->vars_n == 0) return NULL;
+	if (peer->vars_n == 0)
+		return NULL;
 	for(i=peer->vars_n-1;i>=0;i--) {
 		if (keylen != peer->vars[i].keylen) continue;
 		if (!memcmp(key, peer->vars[i].key, keylen)) {
@@ -77,8 +86,10 @@ int64_t vpn_ws_handshake(int queue, vpn_ws_peer *peer) {
 	uint8_t modifier1 = 0;
 	uint8_t modifier2 = 0;
 	ssize_t rlen = vpn_ws_uwsgi_parse(peer, &modifier1, &modifier2);
-	if (rlen < 0) return -1;
-	if (rlen == 0) return 0;
+	if (rlen < 0)
+		return -1;
+	if (rlen == 0)
+		return 0;
 
 	char *remote_addr = vpn_ws_peer_get_var(peer, "REMOTE_ADDR", 11, &peer->remote_addr_len);
 	if (remote_addr) {
@@ -86,9 +97,9 @@ int64_t vpn_ws_handshake(int queue, vpn_ws_peer *peer) {
 	} 
 
 	char *remote_user = vpn_ws_peer_get_var(peer, "REMOTE_USER", 11, &peer->remote_user_len);
-        if (remote_user) {
-                peer->remote_user = vpn_ws_strndup(remote_user, peer->remote_user_len);
-        }
+	if (remote_user) {
+		peer->remote_user = vpn_ws_strndup(remote_user, peer->remote_user_len);
+	}
 
 	char *https_dn = vpn_ws_peer_get_var(peer, "HTTPS_DN", 8, &peer->dn_len);
 	if (https_dn) {
@@ -110,13 +121,15 @@ int64_t vpn_ws_handshake(int queue, vpn_ws_peer *peer) {
 	// now check for websocket request
 	uint16_t ws_key_len = 0;
 	char *ws_key = vpn_ws_peer_get_var(peer, "HTTP_SEC_WEBSOCKET_KEY", 22, &ws_key_len);
-	if (!ws_key) return -1;
+	if (!ws_key)
+		return -1;
 
 	// check if the X-vpn-ws-MAC header is available
 	uint16_t ws_mac_len = 0;
 	char *ws_mac = vpn_ws_peer_get_var(peer, "HTTP_X_VPN_WS_MAC", 17, &ws_mac_len);
 	if (ws_mac) {
-		if (ws_mac_len != 17) return -1;
+		if (ws_mac_len != 17)
+			return -1;
 		uint8_t i;
 		for(i=0;i<6;i++) {
 			ws_mac[(i*3)+2] = 0;
@@ -156,7 +169,7 @@ int64_t vpn_ws_handshake(int queue, vpn_ws_peer *peer) {
 	// encode to base64
 	uint8_t ws_accept[32];
 	uint16_t ws_accept_len = vpn_ws_base64_encode(sha1, 20, ws_accept);
-	
+
 	// append the result to the http response
 
 	memcpy(http_response + sizeof(HTTP_RESPONSE)-1, ws_accept, ws_accept_len);
@@ -164,7 +177,8 @@ int64_t vpn_ws_handshake(int queue, vpn_ws_peer *peer) {
 
 	// send the response
 	int ret = vpn_ws_write(peer, http_response, sizeof(HTTP_RESPONSE)-1 + ws_accept_len + 4);
-	if (ret < 0) return -1;
+	if (ret < 0)
+		return -1;
 	// again ?
 	if (ret == 0) {
 		peer->is_writing = 1;
@@ -194,20 +208,22 @@ static int json_append(char *json, uint64_t *pos, uint64_t *len, char *buf, uint
 static int json_append_num(char *json, uint64_t *pos, uint64_t *len, int64_t n) {
 	char buf[30];	
 	int ret = snprintf(buf, 30, "%lld", (unsigned long long) n);
-	if (ret <= 0 || ret > 30) return -1;
+	if (ret <= 0 || ret > 30)
+		return -1;
 	return json_append(json, pos, len, buf, ret);
 }
 
 static int json_append_mac(char *json, uint64_t *pos, uint64_t *len, uint8_t *mac) {
 	char buf[18];
 	int ret = snprintf(buf, 18, "%02X:%02X:%02X:%02X:%02X:%02X",
-		mac[0],
-		mac[1],
-		mac[2],
-		mac[3],
-		mac[4],
-		mac[5]);
-	if (ret <= 0 || ret > 18) return -1;
+			mac[0],
+			mac[1],
+			mac[2],
+			mac[3],
+			mac[4],
+			mac[5]);
+	if (ret <= 0 || ret > 18)
+		return -1;
 	return json_append(json, pos, len, buf, ret);
 }
 
@@ -215,42 +231,54 @@ static int json_append_json(char *json, uint64_t *pos, uint64_t *len, char *buf,
 	uint64_t i;
 	for(i=0;i<buf_len;i++) {
 		if (buf[i] == '\t') {
-                        if (json_append(json, pos, len, "\\t", 2)) return -1;
-                }
-                else if (buf[i] == '\n') {
-                        if (json_append(json, pos, len, "\\n", 2)) return -1;
-                }
-                else if (buf[i] == '\r') {
-                        if (json_append(json, pos, len, "\\r", 2)) return -1;
-                }
-                else if (buf[i] == '"') {
-                        if (json_append(json, pos, len, "\\\"", 2)) return -1;
-                }
-                else if (buf[i] == '\\') {
-                        if (json_append(json, pos, len, "\\\\", 2)) return -1;
-                }
-                else {
-                        if (json_append(json, pos, len, buf+i, 1)) return -1;
-                }
+			if (json_append(json, pos, len, "\\t", 2))
+				return -1;
+		}
+		else if (buf[i] == '\n') {
+			if (json_append(json, pos, len, "\\n", 2))
+				return -1;
+		}
+		else if (buf[i] == '\r') {
+			if (json_append(json, pos, len, "\\r", 2))
+				return -1;
+		}
+		else if (buf[i] == '"') {
+			if (json_append(json, pos, len, "\\\"", 2))
+				return -1;
+		}
+		else if (buf[i] == '\\') {
+			if (json_append(json, pos, len, "\\\\", 2))
+				return -1;
+		}
+		else {
+			if (json_append(json, pos, len, buf+i, 1))
+				return -1;
+		}
 	}
 	return 0;
 }
 
 /*
-	QUERY_STRING functions
-*/
+   QUERY_STRING functions
+   */
 static char *qs_check(char *qs, uint16_t qs_len, char *key, uint16_t key_len, uint16_t *v_len) {
 	// search for the equal sign
-        char *equal = memchr(qs, '=', qs_len);
-	if (!equal) return NULL;
-	if (key_len != equal-qs) return NULL;
-	if (memcmp(qs, key, key_len)) return NULL;
+	char *equal = memchr(qs, '=', qs_len);
+	if (!equal)
+		return NULL;
+	if (key_len != equal-qs)
+		return NULL;
+	if (memcmp(qs, key, key_len))
+		return NULL;
 	*v_len = qs_len - ((equal-qs)+1);
-	if (!*v_len) return NULL;
+	if (!*v_len)
+		return NULL;
 	return equal+1;
 }
 
 static char *qs_get(char *qs, uint16_t qs_len, char *key, uint16_t key_len, uint16_t *v_len) {
+	assert(qs);
+	assert(key);
 	uint16_t i;
 	char *found = qs;
 	uint16_t found_len = 0;
@@ -259,9 +287,11 @@ static char *qs_get(char *qs, uint16_t qs_len, char *key, uint16_t key_len, uint
 		if (!found) {
 			found = ptr + i;
 		}
+		assert(ptr[i]);
 		if (ptr[i] == '&') {
 			char *value = qs_check(found, found_len, key, key_len, v_len);
-			if (value) return value;
+			if (value)
+				return value;
 			found_len = 0;
 			found = NULL;
 			continue;
@@ -271,14 +301,15 @@ static char *qs_get(char *qs, uint16_t qs_len, char *key, uint16_t key_len, uint
 
 	if (found_len > 0) {
 		char *value = qs_check(found, found_len, key, key_len, v_len);
-		if (value) return value;
+		if (value)
+			return value;
 	}
 	return NULL;
 }
 
 /*
 
-	JSON control interface
+   JSON control interface
 
 */
 
@@ -288,7 +319,8 @@ int64_t vpn_ws_ctrl_json(int queue, vpn_ws_peer *peer) {
 	uint64_t json_pos = 0;
 	uint64_t json_len = 8192;
 	char *json = vpn_ws_malloc(json_len);
-	if (!json) return -1;
+	if (!json)
+		return -1;
 	if (json_append(json, &json_pos, &json_len, HTTP_RESPONSE_JSON, sizeof(HTTP_RESPONSE_JSON)-1)) goto end;
 
 	uint16_t query_string_len = 0;
@@ -315,17 +347,17 @@ int64_t vpn_ws_ctrl_json(int queue, vpn_ws_peer *peer) {
 			}
 			vpn_ws_peer_destroy(b_peer);
 			if (json_append(json, &json_pos, &json_len, "{\"status\":\"ok\"}", 15)) goto end;
-                        goto commit;
+			goto commit;
 		}
 	}
 
 	if (json_append(json, &json_pos, &json_len, "{\"status\":\"ok\",\"peers\":[", 24)) goto end;
 
 	uint8_t found = 0;
-        for(int64_t i = 0; i < vpn_ws_conf.peers_n; i++) {
-                vpn_ws_peer *b_peer = vpn_ws_conf.peers[i];
+	for(int64_t i = 0; i < vpn_ws_conf.peers_n; i++) {
+		vpn_ws_peer *b_peer = vpn_ws_conf.peers[i];
 
-                if (!b_peer) continue;
+		if (!b_peer) continue;
 		if (b_peer->ctrl) continue;
 
 		found = 1;
@@ -386,17 +418,18 @@ int64_t vpn_ws_ctrl_json(int queue, vpn_ws_peer *peer) {
 
 commit:
 	// send the response
-        ret = vpn_ws_write(peer, (uint8_t *)json, json_pos);
-        if (ret < 0) return -1;
-        // again ?
-        if (ret == 0) {
+	ret = vpn_ws_write(peer, (uint8_t *)json, json_pos);
+	if (ret < 0)
+		return -1;
+	// again ?
+	if (ret == 0) {
 		peer->handshake++;
-                peer->is_writing = 1;
-                return vpn_ws_event_read_to_write(queue, peer->fd);
-        }
+		peer->is_writing = 1;
+		return vpn_ws_event_read_to_write(queue, peer->fd);
+	}
 	// force connection close
 end:
 	free(json);
-        return -1;
+	return -1;
 
 }
